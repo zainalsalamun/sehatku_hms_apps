7d0937065bc5554af0f2c9ed33dfb7b4617a9489
zainalsalamun <zainalrtf@gmail.com>
2026-08-28 09:32:44 +0700

    feat: initialize mobile core architecture and implement backend modular services for HMS platform

--- sehatku_hms_backend/src/modules/analytics/analytics.module.ts
+++ sehatku_hms_backend/src/modules/analytics/analytics.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { PharmacyModule } from '../pharmacy/pharmacy.module';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PharmacyModule],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
